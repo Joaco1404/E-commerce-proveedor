f483143b8c9d9bde5cf384c6b0e93404e732bf23
JoaquinGuzman <149431379+Joaco1404@users.noreply.github.com>
2024-12-17 13:46:45 -0300

    Actualizaciones
    En Navbar están los menú despegables de manera correcta y en HeaderTop, los colores de las palabras "+8801xxxxxxxxxx", "Español" y "Conviértete en un vendedor" están un poco mas oscuras

--- src/components/HeaderTop.tsx
+++ src/components/HeaderTop.tsx
@@ -27,63 +27,63 @@ export default function HeaderTop() {
     return (
         <Box className={"bg-white"}>
             <Container maxWidth={"xl"}>
-            <Box className="flex flex-col md:flex-row justify-between items-center p-2 border-b border-white">
-            {/* Sección izquierda */}
-            <Box className="flex items-center mb-2 md:mb-0">
-                <PhoneIcon className="text-[#1455ac] mr-2" />
-                <Typography>+8801xxxxxxxxxx</Typography>
-            </Box>
+                <Box className="flex flex-col md:flex-row justify-between items-center p-2 border-b border-white">
+                    {/* Sección izquierda */}
+                    <Box className="flex items-center mb-2 md:mb-0">
+                        <PhoneIcon className="text-[#1455ac] mr-2" />
+                        <Typography style={{ color: '#0a3d66' }}>+8801xxxxxxxxxx</Typography>
+                    </Box>
 
-            {/* Sección derecha */}
-            <Box className="flex items-center space-x-4">
-                {/* Selección de moneda */}
-                <Box className="flex items-center space-x-1">
-                    <Button
-                        className="py-0 text-[#1455ac]"
-                        aria-controls="currency-menu"
-                        aria-haspopup="true"
-                        onClick={handleCurrencyMenuClick}
-                        endIcon={<ArrowDropDownIcon />}
-                    >
-                        $
-                    </Button>
-                    <Menu
-                        id="currency-menu"
-                        anchorEl={anchorElCurrency}
-                        open={Boolean(anchorElCurrency)}
-                        onClose={handleCurrencyMenuClose}
-                    >
-                        <MenuItem onClick={handleCurrencyMenuClose}>ARS $</MenuItem>
-                        <MenuItem onClick={handleCurrencyMenuClose}>USD $</MenuItem>
-                        <MenuItem onClick={handleCurrencyMenuClose}>EUR €</MenuItem>
-                    </Menu>
+                    {/* Sección derecha */}
+                    <Box className="flex items-center space-x-4">
+                        {/* Selección de moneda */}
+                        <Box className="flex items-center space-x-1">
+                            <Button
+                                className="py-0 text-[#1455ac]"
+                                aria-controls="currency-menu"
+                                aria-haspopup="true"
+                                onClick={handleCurrencyMenuClick}
+                                endIcon={<ArrowDropDownIcon />}
+                            >
+                                $
+                            </Button>
+                            <Menu
+                                id="currency-menu"
+                                anchorEl={anchorElCurrency}
+                                open={Boolean(anchorElCurrency)}
+                                onClose={handleCurrencyMenuClose}
+                            >
+                                <MenuItem onClick={handleCurrencyMenuClose}>ARS $</MenuItem>
+                                <MenuItem onClick={handleCurrencyMenuClose}>USD $</MenuItem>
+                                <MenuItem onClick={handleCurrencyMenuClose}>EUR €</MenuItem>
+                            </Menu>
+                        </Box>
+
+                        {/* Selector de lenguaje */}
+                        <Box className="flex items-center space-x-1">
+                            <IconButton onClick={handleLanguageMenuClick}>
+                                <FlagIcon fontSize="small" />
+                            </IconButton>
+                            <Typography style={{ color: '#0a3d66' }}>Español</Typography>
+                            <IconButton onClick={handleLanguageMenuClick}>
+                                <ArrowDropDownIcon />
+                            </IconButton>
+                            <Menu
+                                id="language-menu"
+                                anchorEl={anchorEllanguage}
+                                open={Boolean(anchorEllanguage)}
+                                onClose={handleLanguageMenuClose}
+                            >
+                                <MenuItem onClick={handleLanguageMenuClose}>Español</MenuItem>
+                                <MenuItem onClick={handleLanguageMenuClose}>Inglés</MenuItem>
+                                <MenuItem onClick={handleLanguageMenuClose}>Francés</MenuItem>
+                            </Menu>
+                        </Box>
+
+                        {/* Botón vendedor */}
+                        <Typography style={{ color: '#0a3d66' }}>Conviértete en un vendedor</Typography>
+                    </Box>
                 </Box>
-
-                {/* Selector de lenguaje */}
-                <Box className="flex items-center space-x-1">
-                    <IconButton onClick={handleLanguageMenuClick}>
-                        <FlagIcon fontSize="small" />
-                    </IconButton>
-                    <Typography>Español</Typography>
-                    <IconButton onClick={handleLanguageMenuClick}>
-                        <ArrowDropDownIcon />
-                    </IconButton>
-                    <Menu
-                        id="language-menu"
-                        anchorEl={anchorEllanguage}
-                        open={Boolean(anchorEllanguage)}
-                        onClose={handleLanguageMenuClose}
-                    >
-                        <MenuItem onClick={handleLanguageMenuClose}>Español</MenuItem>
-                        <MenuItem onClick={handleLanguageMenuClose}>Inglés</MenuItem>
-                        <MenuItem onClick={handleLanguageMenuClose}>Francés</MenuItem>
-                    </Menu>
-                </Box>
-
-                {/* Botón vendedor */}
-                <Typography>Conviértete en un vendedor</Typography>
-            </Box>
-        </Box>
             </Container>
         </Box>
     );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import { Box, Typography, IconButton, Badge, Divider, Container, Menu, MenuItem } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
@@ -7,95 +7,99 @@ import FavoriteIcon from "@mui/icons-material/Favorite";
 import ShoppingBagIcon from "@mui/icons-material/ShoppingBag";
 import StarIcon from "@mui/icons-material/Star";
 
-const Navbar: React.FC =() => {
-    //Estado de cada menú desplegable
+const Navbar: React.FC = () => {
+    // Estado de cada menú desplegable
     const [categoryAnchorEl, setCategoryAnchorEl] = useState<null | HTMLElement>(null);
     const [offersAnchorEl, setOffersAnchorEl] = useState<null | HTMLElement>(null);
     const [storesAnchorEl, setStoresAnchorEl] = useState<null | HTMLElement>(null);
     const [brandsAnchorEl, setBrandsAnchorEl] = useState<null | HTMLElement>(null);
 
-    //Controladores para alternancias desplegables
+    // Controladores para alternancias desplegables
     const handleCategoryClick = (event: React.MouseEvent<HTMLElement>) => {
         setCategoryAnchorEl(event.currentTarget);
     };
     const handleOffersClick = (event: React.MouseEvent<HTMLElement>) => {
         setOffersAnchorEl(event.currentTarget);
-        };
-        const handleStoresClick = (event: React.MouseEvent<HTMLElement>) => {
-            setStoresAnchorEl(event.currentTarget);
-            };
-            const handleBrandsClick = (event: React.MouseEvent<HTMLElement>) => {
-                setBrandsAnchorEl(event.currentTarget);
-                };
-                //Funciones para cerrar menús desplegables
-                const handleClose = () => {
-                    setCategoryAnchorEl(null);
-                    setOffersAnchorEl(null);
-                    setStoresAnchorEl(null);
-                    setBrandsAnchorEl(null);
-                    };
+    };
+    const handleStoresClick = (event: React.MouseEvent<HTMLElement>) => {
+        setStoresAnchorEl(event.currentTarget);
+    };
+    const handleBrandsClick = (event: React.MouseEvent<HTMLElement>) => {
+        setBrandsAnchorEl(event.currentTarget);
+    };
+
+    // Funciones para cerrar menús desplegables
+    const handleClose = () => {
+        setCategoryAnchorEl(null);
+        setOffersAnchorEl(null);
+        setStoresAnchorEl(null);
+        setBrandsAnchorEl(null);
+    };
 
     return (
         <Box className="bg-white">
             <Container maxWidth={"xl"}>
-            <Box className="flex flex-warp items-center justify-between bg-white p-2 shadow-md">
-            {/*Seccion izquierdo - Menu despegable de categorias*/}
-            <Box className="flex items-center bg-blue 700 p-2 w-full md:w-auto">
-                <IconButton className="text-white">
-                    <MenuIcon />
-                </IconButton>
-                <Typography className="text-white ml-2">Seleccionar categorías</Typography>
-                <IconButton className="text-white">
-                    <ExpandMoreIcon />
-                </IconButton>
-                {/*Seleccion de menu despegable de categorias*/}
-                <Menu anchorEl={categoryAnchorEl} open={Boolean(categoryAnchorEl)} onClose={handleCategoryClick}>
-                    <MenuItem onClick={handleClose}>Categoría 1</MenuItem>
-                    <MenuItem onClick={handleClose}>Categoría 2</MenuItem>
-                    <MenuItem onClick={handleClose}>Categoría 3</MenuItem>
-                </Menu>
-            </Box>
+                <Box className="flex flex-warp items-center justify-between bg-white p-2 shadow-md">
+                    {/* Sección izquierdo - Menú desplegable de categorías */}
+                    <Box className="flex items-center bg-blue-700 p-2 w-full md:w-auto">
+                        <IconButton className="text-white">
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography className="text-white ml-2">Seleccionar categorías</Typography>
+                        <IconButton className="text-white">
+                            <ExpandMoreIcon />
+                        </IconButton>
+                        {/* Selección de menú desplegable de categorías */}
+                        <Menu anchorEl={categoryAnchorEl} open={Boolean(categoryAnchorEl)} onClose={handleCategoryClick}>
+                            <MenuItem onClick={handleClose}>Categoría 1</MenuItem>
+                            <MenuItem onClick={handleClose}>Categoría 2</MenuItem>
+                            <MenuItem onClick={handleClose}>Categoría 3</MenuItem>
+                        </Menu>
+                    </Box>
 
-            {/*Seccion medio - Navegacion de enlaces*/}
-            <Box className="flex flex-wrap items-center space-x-4 mt-2 md:mt-0 w-full md:w-auto">
-                <Typography component="a" href="#" className="text-gray-700">HOME</Typography>
+                    {/* Sección medio - Navegación de enlaces */}
+                    <Box className="flex flex-wrap items-center space-x-4 mt-2 md:mt-0 w-full md:w-auto">
+                        <Typography component="a" href="#" className="text-gray-700">HOME</Typography>
 
-                {/*Menu despegable de ofertas*/}
-                <Typography component="a" href="#" className="text-gray-700 flex items-center" onClick={handleOffersClick}>Ofertas
-                <ExpandMoreIcon className="ml-1" fontSize="small" />
-                </Typography>
-                <Menu anchorEl={offersAnchorEl} open={Boolean(offersAnchorEl)} onClose={handleOffersClick}>
-                    <MenuItem onClick={handleClose}>Oferta 1</MenuItem>
-                    <MenuItem onClick={handleClose}>Oferta 2</MenuItem>
-                    <MenuItem onClick={handleClose}>Oferta 3</MenuItem>
-                </Menu>
+                        {/* Menú desplegable de ofertas */}
+                        <Typography component="a" href="#" className="text-gray-700 flex items-center" onClick={handleOffersClick}>Ofertas
+                            <ExpandMoreIcon className="ml-1" fontSize="small" />
+                        </Typography>
+                        <Menu anchorEl={offersAnchorEl} open={Boolean(offersAnchorEl)} onClose={handleOffersClick}>
+                            <MenuItem onClick={handleClose}>Oferta 1</MenuItem>
+                            <MenuItem onClick={handleClose}>Oferta 2</MenuItem>
+                            <MenuItem onClick={handleClose}>Oferta 3</MenuItem>
+                        </Menu>
 
-                {/*Menu despegable de tiendas*/}
-                <Typography component="a" href="#" className="text-gray-700 flex items-center" onClick={handleStoresClick}>
-                    Tiendas
-                    <ExpandMoreIcon className="ml-1" fontSize="small" />
-                </Typography>
-                <MenuItem onClick={handleClose}>Tienda 1</MenuItem>
-                <MenuItem onClick={handleClose}>Tienda 2</MenuItem>
-                <MenuItem onClick={handleClose}>Tienda 3</MenuItem>
+                        {/* Menú desplegable de tiendas */}
+                        <Typography component="a" href="#" className="text-gray-700 flex items-center" onClick={handleStoresClick}>
+                            Tiendas
+                            <ExpandMoreIcon className="ml-1" fontSize="small" />
+                        </Typography>
+                        <Menu anchorEl={storesAnchorEl} open={Boolean(storesAnchorEl)} onClose={handleStoresClick}>
+                            <MenuItem onClick={handleClose}>Tienda 1</MenuItem>
+                            <MenuItem onClick={handleClose}>Tienda 2</MenuItem>
+                            <MenuItem onClick={handleClose}>Tienda 3</MenuItem>
+                        </Menu>
 
-                {/*Menu despegable de marcas*/}
-                <Typography component="a" href="#" className="text-gray-700 flex items-center" onClick={handleBrandsClick}>
-                    Marcas
-                    <ExpandMoreIcon className="ml-1" fontSize="small" />
-                </Typography>
-                <Menu anchorEl={storesAnchorEl} open={Boolean(storesAnchorEl)} onClose={handleStoresClick}>
-                    <MenuItem onClick={handleClose}>Marca 1</MenuItem>
-                    <MenuItem onClick={handleClose}>Marca 2</MenuItem>
-                    <MenuItem onClick={handleClose}>Marca 3</MenuItem>
-                </Menu>
+                        {/* Menú desplegable de marcas */}
+                        <Typography component="a" href="#" className="text-gray-700 flex items-center" onClick={handleBrandsClick}>
+                            Marcas
+                            <ExpandMoreIcon className="ml-1" fontSize="small" />
+                        </Typography>
+                        <Menu anchorEl={brandsAnchorEl} open={Boolean(brandsAnchorEl)} onClose={handleBrandsClick}>
+                            <MenuItem onClick={handleClose}>Marca 1</MenuItem>
+                            <MenuItem onClick={handleClose}>Marca 2</MenuItem>
+                            <MenuItem onClick={handleClose}>Marca 3</MenuItem>
+                        </Menu>
 
+                    
                 {/*Productos en descuento*/}
                 <Typography component="a" href="#" className="text-gray-700 flex items-center">
                     Productos en descuento
                     <StarIcon className="text-yellow-500 ml-1" fontSize="small"/>
                 </Typography>
-            </Box>
+                    </Box>
 
             {/*Seccion derecho - Iconos y uso de acciones*/}
             <Box className="flex items-center space x-4 mt-2 md:mt-0 w-full md:w-auto justify-center">
@@ -107,18 +111,24 @@ const Navbar: React.FC =() => {
                 <SyncAltIcon />
             </IconButton>
             <IconButton className="text-gray-700">
-                <FavoriteIcon />
-            </IconButton>
+                                <FavoriteIcon />
+
+                        </IconButton>
             <Divider orientation="vertical" flexItem className="border-gray-300" />
             <Box className="relative">
                 <IconButton className="text-gray-700">
                     <Badge badgeContent={0} color="primary">
-                        <ShoppingBagIcon />
-                    </Badge>
-                </IconButton>
+                                <ShoppingBagIcon />
+                            </Badge>
+                        </IconButton>
             </Box>
-            </Box>
-        </Box>
+
+
+
+
+
+                    </Box>
+                </Box>
             </Container>
         </Box>
     );
